80bfd8f450fd26ee9466cb132e6216c9c7ae3d09
Lewis <lewis.fsantos@gmail.com>
2020-05-15 18:38:54 -0300

    Remove unnecessary console.log call from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,3 @@
-// console.log('client side js file is loaded');
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     });
-// });
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error) {
-//             console.log(data.error);
-//             return;
-//         }
-
-//         console.log(data.location);
-//         console.log(data.forecast);
-//     });
-// });
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
